578d56655f3325688628d2494482fd45175923ff
guan zhong <catyguan@gmail.com>
2014-01-17 18:11:33 +0800

    client

--- src/esp/xmem/xmemclient/espnetClient.go
+++ src/esp/xmem/xmemclient/espnetClient.go
@@ -1,1 +1,13 @@
 package xmem
+
+import "esp/espnet"
+
+type Client struct {
+	c *espnet.ChannelClient
+}
+
+func NewClient(c *espnet.ChannelClient) Client {
+	this := new(Client)
+	this.c = c
+	return this
+}
