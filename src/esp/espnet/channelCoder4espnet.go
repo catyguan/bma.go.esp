b1c69b940136717367f640b3eef4a00abad1e910
guan zhong <catyguan@gmail.com>
2014-01-21 18:49:56 +0800

    nodeid

--- src/esp/espnet/channelCoder4espnet.go
+++ src/esp/espnet/channelCoder4espnet.go
@@ -12,7 +12,7 @@ type ChannelCoder4Espnet struct {
 }
 
 func (this *ChannelCoder4Espnet) Init() {
-	this.maxframe = 1024 * 1024
+	this.maxframe = 10 * 1024 * 1024
 	this.reader = protpack.NewPackageReader()
 }
 
