fc0297c37b9d207c3edaaecc2f85ef7d504a0c8c
guan zhong <catyguan@gmail.com>
2014-08-06 09:50:50 +0800

    glua1h

--- src/app/glua1h/main.go
+++ src/app/glua1h/main.go
@@ -6,6 +6,7 @@ import (
 	"esp/glua/httpmux4glua"
 	"httpserver"
 	"net/http"
+	"os"
 )
 
 const (
@@ -13,14 +14,18 @@ const (
 )
 
 func main() {
-	cfile := "config/glua1h-config.json"
+	cfile := "config/glua4h-config.json"
 
 	service := glua.NewService("gluaService")
 	boot.Add(service, "", false)
 
+	var wd, _ = os.Getwd()
+
 	mux := http.NewServeMux()
+	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir(wd+"/public"))))
+
 	mux4gl := httpmux4glua.NewService("gluaMux", service)
-	mux4gl.InitMux(mux, "/")
+	mux4gl.InitMux(mux, "/i/")
 	boot.AddService(mux4gl)
 
 	httpService := httpserver.NewHttpServer("httpPoint", mux)
